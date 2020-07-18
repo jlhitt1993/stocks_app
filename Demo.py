6836b5cc88af6d6383d7c37ef1c70979229355d8
Jeremy Hitt <jlhitt1993@gmail.com>
2020-07-18 15:27:29 -0400

    adding new jsons and script for saving new ones

--- Demo.py
+++ Demo.py
@@ -3,12 +3,14 @@
 from OO_stocks import Stock, candlestick, correlation, percent_change, \
     fourier, spectrum, help
 
-AAPL = Stock('aapl')
-AMD = Stock('amd')
-MSFT = Stock('msft')
-candlestick(AMD)
-correlation(stocks=[AAPL, AMD, MSFT], labels=['high', 'low', 'open'])
-percent_change(stocks=[AAPL, AMD, MSFT], labels=['high', 'low', 'open'])
-spectrum(stocks=[AAPL, AMD, MSFT], labels=['high', 'low', 'open'])
-fourier(stocks=[AAPL, AMD, MSFT], labels=['high', 'low', 'open'])
+
+AAPL = Stock('aapl', local="C:/Users/Jeremy/stocks_app/")
+AMD = Stock('amd', local="C:/Users/Jeremy/stocks_app/")
+MSFT = Stock('msft', local="C:/Users/Jeremy/stocks_app/")
+LMT = Stock('lmt', local="C:/Users/Jeremy/stocks_app/")
+#candlestick(AMD)
+correlation(stocks=[AAPL, AMD, MSFT, LMT], labels=['high', 'low', 'open', 'open'])
+percent_change(stocks=[AAPL, AMD, MSFT, LMT], labels=['high', 'low', 'open', 'open'])
+spectrum(stocks=[AAPL, AMD, MSFT, LMT], labels=['high', 'low', 'open', 'open'])
+fourier(stocks=[AAPL, AMD, MSFT, LMT], labels=['high', 'low', 'open', 'open'])
 # help()
