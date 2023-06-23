4018e6e61835928d281d9adc1718db10b9467e65
Jeremy Hitt <jlhitt1993@gmail.com>
2023-06-22 20:55:34 -0400

    added github to help function

--- stocks_app/help.py
+++ stocks_app/help.py
@@ -5,4 +5,4 @@ def help():
     print('Welcome to help.\nStart by getting data about a stock ex. aapl = Stock(\'aapl\')')
     print('You can make plots by spectrum(aapl, \'high\', amd, \'high\'). Other plots are correlation(),'
           'percent_change(), candlestick and fourier()')
-    print('For more help, see the documentation and examples at www.placeholder.com')
+    print('For more help, see the documentation and examples at https://github.com/jlhitt1993/stocks_app/blob/master/README.md')
